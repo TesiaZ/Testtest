3e10f4be2088e9b355a2642c644f85e0931d095c
tesia <tesiazheng1994@gmail.com>
2025-12-18 11:17:32 -0600

    test code

---
+++ test/test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  test
+//
+//  Created by Tesia on 12/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ test/testTests/testTests.swift
@@ -0,0 +1,16 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Tesia on 12/18/25.
+//
+
+import Testing
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
